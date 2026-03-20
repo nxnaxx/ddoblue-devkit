fbe401fd78dc4eb9a7861c256831467ba9398e88
nxnaxx <nxnaxx125@gmail.com>
2026-03-20 18:07:19 +0900

    feat(prettier-config): add shared prettier configuration package

---
+++ packages/prettier-config/index.mjs
@@ -0,0 +1,24 @@
+/** @type {import("prettier").Config} */
+const config = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+
+  overrides: [
+    {
+      files: '*.md',
+      options: {
+        printWidth: 80,
+      },
+    },
+  ],
+};
+
+export default config;
