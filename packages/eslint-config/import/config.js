deaad7b5ca073c4da48a8a1e41174ff85fc04651
nxnaxx <nxnaxx125@gmail.com>
2026-03-23 14:51:16 +0900

    feat(eslint-config): add import config

---
+++ packages/eslint-config/import/config.js
@@ -0,0 +1,25 @@
+import importPlugin from 'eslint-plugin-import';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import unusedImports from 'eslint-plugin-unused-imports';
+
+import { importRules } from './rules/core.js';
+import { sortRules } from './rules/sort.js';
+import { unusedImportRules } from './rules/unused.js';
+
+export const importConfig = [
+  {
+    name: 'ddoblue/import',
+
+    plugins: {
+      import: importPlugin,
+      'simple-import-sort': simpleImportSort,
+      'unused-imports': unusedImports,
+    },
+
+    rules: {
+      ...importRules,
+      ...sortRules,
+      ...unusedImportRules,
+    },
+  },
+];
