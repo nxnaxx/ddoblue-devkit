72ec17a929730888cda79321d01f0ec9779894c3
nxnaxx <nxnaxx125@gmail.com>
2026-03-31 21:24:33 +0900

    refactor(root): centralize lint-staged configuration in root

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,37 @@
-import config from '@ddoblue/eslint-config';
+import baseConfig from '@ddoblue/eslint-config';
+import nestConfig from '@ddoblue/eslint-config/presets/nest';
+import nextConfig from '@ddoblue/eslint-config/presets/next-strict';
+import reactConfig from '@ddoblue/eslint-config/presets/react-strict';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
-export default [
-  ...config,
+const eslintConfig = defineConfig([
+  ...baseConfig,
 
-  {
-    ignores: [
-      'dist',
-      'node_modules',
-      '**/commitlint.config.ts',
-      '**/*.d.ts',
-      '**/*.cjs',
-      'apps/*',
-      'packages/*',
-    ],
-  },
-];
+  ...nextConfig.map((config) => ({
+    ...config,
+    files: ['apps/next/**/*.{js,mjs,ts,tsx}'],
+  })),
+
+  ...reactConfig.map((config) => ({
+    ...config,
+    files: ['apps/react/**/*.{js,mjs,ts,tsx}'],
+  })),
+
+  ...nestConfig.map((config) => ({
+    ...config,
+    files: ['apps/nest/**/*.{js,mjs,ts}'],
+  })),
+
+  globalIgnores([
+    '**/node_modules/**',
+    '**/dist/**',
+    '**/coverage/**',
+    '**/.cache/**',
+    '**/.turbo/**',
+    '**/*.d.ts',
+    'commitlint.config.ts',
+    '**/tsup.config.ts',
+  ]),
+]);
+
+export default eslintConfig;
